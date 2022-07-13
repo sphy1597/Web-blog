e071f4140a56dd3ae2560703f551a681225e19a7
sphy1597 <101171867+sphy1597@users.noreply.github.com>
2022-07-13 16:15:25 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".outer-menu-item").hover(
+    function () {
+      $(this).find(".inner-menu").show();
+    },
+    function () {
+      $(this).find(".inner-menu").hide();
+    }
+  );
+});
